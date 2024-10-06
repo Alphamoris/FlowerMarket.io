71739cefb0a69fc908e896f969b9a0e79164331f
Alphamoris <alphamoris45@gamil.com>
2024-10-06 14:45:39 +0530

    deleted some files

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,23 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from . import comments, price_details , signup , signin , image , imgsend
 
-app=FastAPI()
+app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins (for development)
-    allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
-)
-
-
-
-app.include_router(price_details.router , prefix= "/items")
-app.include_router(comments.router , prefix= "/comment")
-app.include_router(signup.router , prefix= "/signup")
-app.include_router(signin.router , prefix= "/signin")
-app.include_router(image.router , prefix= "/image")
-app.include_router(imgsend.router , prefix= "/imgsend")
-
+@app.get("/")
+def f_func():
+    return {"msg" : "This is the good time to start the development go ahead and rock Mr. Alpha Moris !!!!!"}
